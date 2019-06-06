a845f9584e1d6d597920eb01ce16282bc23468e4
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-06-06 20:09:15 +0300

    ad

--- Shaped/MainWindow.xaml.cs
+++ Shaped/MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace Shaped
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BlackNWhiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a linear gradient brush with five stops   
+            LinearGradientBrush blacknwhiteBrush = new LinearGradientBrush();
+            blacknwhiteBrush.StartPoint = new Point(0, 0);
+            blacknwhiteBrush.EndPoint = new Point(1, 1);
+            // Create and add Gradient stops  
+            GradientStop blackGS = new GradientStop();
+            blackGS.Color = Colors.Black;
+            blackGS.Offset = 0.0;
+            blacknwhiteBrush.GradientStops.Add(blackGS);
+            GradientStop whiteGS = new GradientStop();
+            whiteGS.Color = Colors.White;
+            whiteGS.Offset = 1.0;
+            blacknwhiteBrush.GradientStops.Add(whiteGS);
+            UIPath.Fill = blacknwhiteBrush;
         }
     }
 }
